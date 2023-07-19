1df79cd22d842151f0a1ca73f067e597069f8c87
mertcandav <mertcandav@outlook.com>
2023-07-19 10:32:16 +0300

    add package script for package extension

---
+++ package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+# Copyright 2023 The Jule Programming Language.
+# Use of this source code is governed by a BSD 3-Clause
+# license that can be found in the LICENSE file.
+
+./node_modules/@vscode/vsce/vsce package
